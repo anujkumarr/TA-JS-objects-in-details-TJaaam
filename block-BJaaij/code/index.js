212fbe460751ab4bd07dbcd7f24d09cf1c0a8793
akk26 <anujbph@gmail.com>
2021-09-15 23:00:42 +0530

    After refactor

--- block-BJaaij/code/index.js
+++ block-BJaaij/code/index.js
@@ -1,45 +1,52 @@
 // book
 
-  class Book{
-    constructor(title, category, author, isRead= false, finishedDate) {
-      this.title = title;
-      this.category = category;
-      this.author = author;
-      this.isRead = isRead;
-      this.finishedDate = finishedDate;
-    }
-    
-    markBookAsRead() {
-      this.isRead = true;
-      this.finishedDate = Date.now();
-    }
+class Book {
+  constructor(title, category, author) {
+    this.title = title;
+    this.category = category;
+    this.author = author;
+    this.isRead = false;
+    this.finishedDate = null;
   }
-
-  class BookList{
-    constructor() {
-      this.allBooks = [];
-      this.index = 0;
-    }
-
-    add(bookArr) {
-      return this.allBooks.push(bookArr);
-    }
-
-    getCurrentBook(index) {
-      return this.allBooks[this.index];
-    }
-
-    getNextBook(index) {
-      return this.allBooks[this.index + 1];
-    }
-
-    getNextBook(index) {
-      return this.allBooks[this.index - 1];
-    }
-
-    changeCurrentBook(value) {
-      return this.index = 0;
-    }
-
-
+  markBookAsRead() {
+    this.isRead = true;
+    this.finishedDate = Date.now();
   }
+}
+
+
+  // booklist
+
+class Booklist {
+  constructor() {
+    this.AllBooks = [];
+    this.index = 0;
+  }
+  add(arr = []) {
+    this.AllBooks = this.AllBooks.concat(arr);
+    return this.AllBooks;
+  }
+  getCurrentBook() {
+    return this.AllBooks[this.index];
+  }
+  getNextBook() {
+    return this.AllBooks[this.index + 1];
+  }
+  getPrevBook() {
+    return this.AllBooks[this.index - 1];
+  }
+  changeCurrentBook(num) {
+    this.index = num;
+  }
+}
+
+let book1 = new Book("The Old Man and the Sea", "Allegory, Nautical fiction", "Ernest Hemigway");
+
+let book2 = new Book("Harry Potter", "Fantasy", "J K Rowling");
+let book3 = new Book("Lord of the rings", "Fantasy", "Tolkins");
+let book4 = new Book("Wings of fire", "Autobiography", "A P J");
+let book5 = new Book("Animal Farm", "Political Satire", "Gorge Orewell");
+
+let library = new Booklist();
+
+library.add([book1, book2, book3, book4, book5]);
